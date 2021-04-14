25d770a46dec0cdf3b9b89fe79cb96f52ead6b7b
eutyupin <82430722+eutyupin@users.noreply.github.com>
2021-04-14 13:50:28 +0700

    Update src/Lesson1/HomeWorkApp.java
    Co-authored-by: OKrylov <krylov92oa@gmail.com>

--- src/Lesson1/HomeWorkApp.java
+++ src/Lesson1/HomeWorkApp.java
@@ -21,7 +21,7 @@ public class HomeWorkApp {
         int value = 45;
         if (value <= 0){
             System.out.println("Красный");
-        } else if (value > 0 && value<= 100){
+        } else if (value > 0 && value <= 100) {
             System.out.println("Желтый");
         } else if (value > 100){
             System.out.println("Зеленый");
