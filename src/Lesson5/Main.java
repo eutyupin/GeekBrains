865ee00f046bb7e4c80e7e6a693fd6f58e9f8e25
eutyupin <82430722+eutyupin@users.noreply.github.com>
2021-05-03 11:31:33 +0700

    Update src/Lesson5/Main.java
    Co-authored-by: OKrylov <krylov92oa@gmail.com>

--- src/Lesson5/Main.java
+++ src/Lesson5/Main.java
@@ -19,7 +19,7 @@ public class Main {
 
     private static void printAllPersons(Worker[] person) {
         for(int personCount = 0; personCount < person.length; personCount++){
-           if (person[personCount].getAge() > 40) System.out.println(person[personCount].toString());
+           if (person[personCount].getAge() > 40) System.out.println(person[personCount]);
         }
     }
 
