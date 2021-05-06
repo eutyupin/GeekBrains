0b7fe02c7b8b64deb80e667d9e3eb5c93787a056
Evgeniy <eu-1982@yandex.ru>
2021-05-06 20:27:02 +0700

    HomeWork7

---
+++ src/Lesson7/Cat.java
@@ -0,0 +1,35 @@
+package Lesson7;
+
+public class Cat {
+    private String name;
+    private  int appetite;
+    private boolean well_fed;
+    public Cat(String name, int appetite) {
+        this.name = name;
+        this.appetite = appetite;
+        this.well_fed = false;
+    }
+    public void eat(Plate p) {
+        if (well_fed == false && p.getFood() >= appetite) {
+            p.decreaseFood(appetite);
+            well_fed = true;
+            System.out.println("Кот " + name + " наелся, съел " + appetite);
+        } else checkFood(p);
+    }
+    private void checkFood(Plate p) {
+        if (p.getFood() < appetite && p.getFood() > 0) System.out.println("В тарелке еды меньше, чем аппетит кота " + name + " : " + appetite);
+        else if (p.getFood() == 0) System.out.println("Тарелка пуста, для кота " + name + " еды не хватило :(");
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAppetite() {
+        return appetite;
+    }
+
+    public boolean getWell_fed() {
+        return well_fed;
+    }
+}
---
+++ src/Lesson7/TaskStrings.java
@@ -0,0 +1,36 @@
+package Lesson7;
+
+import java.util.Locale;
+
+public class TaskStrings {
+    private static final String EXAMPLE_STRING = "I like Java!!!";
+    public static void main(String[] args) {
+        //1
+        receiveString(EXAMPLE_STRING);
+        //2
+        System.out.println(EXAMPLE_STRING.charAt(EXAMPLE_STRING.length()-1));
+        //3
+        System.out.println(EXAMPLE_STRING.endsWith("!!!"));
+        //4
+        System.out.println(EXAMPLE_STRING.startsWith("I like"));
+        //5
+        System.out.println(EXAMPLE_STRING.contains("Java"));
+        //6
+        System.out.println(EXAMPLE_STRING.indexOf("Java"));
+        //7
+        System.out.println(EXAMPLE_STRING.replaceAll("a","o"));
+        //8
+        System.out.println(EXAMPLE_STRING.toUpperCase());
+        //9
+        System.out.println(EXAMPLE_STRING.toLowerCase());
+        //10
+        System.out.println(EXAMPLE_STRING.substring(EXAMPLE_STRING.indexOf("Java"), EXAMPLE_STRING.indexOf("Java") + 4));
+
+
+    }
+
+    public static void receiveString(String s){
+        System.out.println(s);
+    }
+
+}
